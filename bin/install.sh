63558eb75090766ad6b867198e9ffca97625e7e9
Mike Barkmin <mike@barkmin.eu>
2021-11-16 23:46:47 +0100

    automagic message

--- bin/install.sh
+++ bin/install.sh
@@ -7,21 +7,34 @@ if [[ "$PROFILE" != "work" && "$PROFILE" != "personal" && "PROFILE" != "demo" ]]
   exit 1
 fi
 
+echo "** Installing core packages **"
+rpm-ostree install --idempotent --allow-inactive --apply-live ansible neovim zsh flatpak-builder git python3 python3-psutil stow tmux fzf wl-clipboard ripgrep git-crypt
+
+echo "** Removing firefox from base image **"
+rpm-ostree override remove firefox 2>/dev/null
+
 pushd $HOME
-  rpm-ostree install --idempotent --allow-inactive --apply-live ansible neovim zsh flatpak-builder git python3 python3-psutil stow tmux fzf wl-clipboard ripgrep git-crypt
-  rpm-ostree override remove firefox 2>/dev/null
-  git clone git@github.com:mikebarkmin/.dotfiles.git --recursive
-  git submodule foreach --recursive git checkout main
-  git submodule foreach 'test -d .git-crypt && git-crypt unlock || return 0'
+  echo "** Clone dotfiles **"
+  git clone git@github.com:mikebarkmin/.dotfiles.git .dotfiles
 
   pushd .dotfiles
+    echo "** Setup profile $PROFILE"
+    git submodule update --init $PROFILE
+    pushd $PROFILE
+      git checkout main
+      if [[ -d ".git-crypt" ]]; then
+        git-crypt unlock
+      fi
+    popd
     ./install-$PROFILE
   popd
 
   mkdir -p $HOME/Sources
+  echo "** Clone silberblau **"
   git clone git@github.com:mikebarkmin/silberblau.git
 
   pushd ansible
+    echo "** Run playbook **"
     ansible-galaxy install -r requirements.yml
     ansible-playbook main.yml --ask-become
   popd
