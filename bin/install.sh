f1f1224479cba869fd3200e7fddeb6a17f948d13
Mike Barkmin <mike.barkmin@uni-due.de>
2021-12-10 10:50:26 +0100

    automagic message

--- bin/install.sh
+++ bin/install.sh
@@ -10,8 +10,11 @@ fi
 echo "** Installing core packages **"
 rpm-ostree install --idempotent --allow-inactive --apply-live ansible neovim zsh flatpak-builder git python3 python3-psutil stow tmux fzf wl-clipboard ripgrep git-crypt
 
+echo "** Installing sway packages **"
+rpm-ostree install --idempotent --allow-inactive --apply-live light sway swaylock waybar ulauncher mako kanshi lxpolkit
+
 echo "** Removing firefox from base image **"
-rpm-ostree override remove firefox 2>/dev/null
+rpm-ostree override remove firefox 2> /dev/null
 
 if [[ -n "$NVIDIA" ]]; then
   echo "** Installing NVIDIA drivers **"
@@ -22,28 +25,28 @@ if [[ -n "$NVIDIA" ]]; then
 fi
 
 pushd $HOME
-  echo "** Clone dotfiles **"
-  git clone git@github.com:mikebarkmin/.dotfiles.git .dotfiles
+echo "** Clone dotfiles **"
+git clone git@github.com:mikebarkmin/.dotfiles.git .dotfiles
 
-  pushd .dotfiles
-    echo "** Setup profile $PROFILE"
-    git submodule update --init $PROFILE
-    pushd $PROFILE
-      git checkout main
-      if [[ -d ".git-crypt" ]]; then
-        git-crypt unlock
-      fi
-    popd
-    ./install-$PROFILE
-  popd
+pushd .dotfiles
+echo "** Setup profile $PROFILE"
+git submodule update --init $PROFILE
+pushd $PROFILE
+git checkout main
+if [[ -d ".git-crypt" ]]; then
+  git-crypt unlock
+fi
+popd
+./install-$PROFILE
+popd
 
-  mkdir -p $HOME/Sources
-  echo "** Clone silberblau **"
-  git clone git@github.com:mikebarkmin/silberblau.git
+mkdir -p $HOME/Sources
+echo "** Clone silberblau **"
+git clone git@github.com:mikebarkmin/silberblau.git
 
-  pushd ansible
-    echo "** Run playbook **"
-    ansible-galaxy install -r requirements.yml
-    ansible-playbook main.yml --ask-become
-  popd
+pushd ansible
+echo "** Run playbook **"
+ansible-galaxy install -r requirements.yml
+ansible-playbook main.yml --ask-become
+popd
 popd
