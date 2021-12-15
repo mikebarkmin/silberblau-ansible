0190ae8555ea665f87a11a331ca41e82c6c2af3e
Mike Barkmin <mike.barkmin@uni-due.de>
2021-12-15 11:18:17 +0100

    automagic message

--- bin/install.sh
+++ bin/install.sh
@@ -14,7 +14,7 @@ echo "** Installing core packages **"
 rpm-ostree install --idempotent --allow-inactive --apply-live ansible neovim zsh flatpak-builder git python3 python3-psutil stow tmux fzf wl-clipboard ripgrep git-crypt alacritty
 
 echo "** Installing sway packages **"
-rpm-ostree install --idempotent --allow-inactive --apply-live light sway swaylock waybar ulauncher mako kanshi lxpolkit blueberry pavucontrol playerctl libappindicator-gtk3 xdg-desktop-portal-wlr wf-recorder
+rpm-ostree install --idempotent --allow-inactive --apply-live light sway swaylock waybar ulauncher mako kanshi lxpolkit blueberry pavucontrol playerctl libappindicator-gtk3 xdg-desktop-portal-wlr wf-recorder pulseaudio-utils network-manager-applet
 
 echo "** Install V4l2loopback"
   rpm-ostree install akmod-v4l2loopback
