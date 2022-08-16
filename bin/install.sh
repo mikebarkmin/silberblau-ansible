f043be96ad649187e0a4d35fb2d54750a50f4380
Mike Barkmin <mike@barkmin.eu>
2022-08-16 09:46:56 +0200

    automagic message

--- bin/install.sh
+++ bin/install.sh
@@ -10,6 +10,9 @@ fi
 echo "** Enable RPM Fusion **"
 rpm-ostree install --idempotent --apply-live https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+echo "** Enable Virtio **"
+rpm-ostree install --idempotent --apply-live https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.noarch.rpm
+
 echo "** Installing core packages **"
 rpm-ostree install --idempotent --allow-inactive --apply-live ansible neovim zsh flatpak-builder git python3 python3-psutil stow tmux fzf wl-clipboard ripgrep git-crypt pavucontrol pulseaudio-utils ulauncher openssl nautilus-python
 
